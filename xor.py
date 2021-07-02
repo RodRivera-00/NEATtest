08b0f2be5cce492e24a07d293fbd1bc9e99c283c
admin@tissue.finance <admin@yeonin.io>
2021-07-03 03:39:16 +0800

    fixed

--- xor.py
+++ xor.py
@@ -113,7 +113,7 @@ def eval_genome(genome, config):
             else:
                 #if round(trade[0]) != 0:
                     #balance -= 1
-                if round(trade) == 0 and pnl != 0 and ticks > 30:
+                if round(trade) == 0 and pnl != 0 and ticks > 15:
                 #add reward for every closing trade
                 #balance += 1
                     openprice = 0
