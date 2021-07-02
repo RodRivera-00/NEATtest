d275fc69de0bafce5d53aacb3018ac6945f2d268
admin@tissue.finance <admin@yeonin.io>
2021-07-02 20:42:29 +0800

    fixed

--- xor.py
+++ xor.py
@@ -149,9 +149,6 @@ def run():
     # Save the winner.
     with open('winner', 'wb') as f:
         pickle.dump(winner, f)
-    visualize.draw_net(config, winner, True, node_names=node_names)
-    visualize.plot_stats(stats, ylog=False, view=True)
-    visualize.plot_species(stats, view=True)
     print(winner)
 
 
